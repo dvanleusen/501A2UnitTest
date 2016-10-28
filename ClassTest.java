df4e62da6ecf1e3ec7869d45ddf3efb6de0f656b
dvanleusen <danvanleusen@yahoo.co.uk>
2016-10-28 09:50:21 -0600

    Version 8: included ClassTest.java
    - ClassTest.java is used to test fields for version 7

---
+++ ClassTest.java
@@ -0,0 +1,10 @@
+public class ClassTest {
+ 
+    public ClassTest() {  }
+
+    public void runtest() { }
+
+    private int[] valArray={1,2,3};
+    private ClassA valClass=new ClassA();
+    private ClassA[] vallarray = new ClassA[10];
+}
