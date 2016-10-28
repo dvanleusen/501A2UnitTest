16cd9f8679c5756e727869ca97942612a1a699c2
dvanleusen <danvanleusen@yahoo.co.uk>
2016-10-27 22:44:21 -0600

    Version 2: Added tests for testing finding the name of the immediate superclass
    Added tests for testing finding the name of the immediate superclass
    with 2 scenarios

--- UnitTest.java
+++ UnitTest.java
@@ -13,11 +13,10 @@ import java.lang.reflect.*;
 
 public class UnitTest {
    
-@Test
-//test declaring class name
+	@Test
+	// test declaring class name
     public void testDeclaringClass() {
-
-		//Declaring class is ClassA, "ClassA" is expected
+		// declaring class is ClassA, "ClassA" is expected
         Object obj = new ClassA();
         Class objClass = obj.getClass();
         Inspector objInspector = new Inspector();
@@ -25,13 +24,35 @@ public class UnitTest {
         String dClass=objInspector.inspectDeclaringClass(m[0]);
         org.junit.Assert.assertEquals(dClass, "ClassA");
         
-        //Declaring class is ClassD, "ClassD" is expected
+        // declaring class is ClassD, "ClassD" is expected
         obj = new ClassD();
         objClass = obj.getClass();
-        objInspector = new Inspector();
         m = objClass.getMethods();
         dClass=objInspector.inspectDeclaringClass(m[0]);
         org.junit.Assert.assertEquals(dClass, "ClassD");
 
+        //
     }
+
+	@Test  
+	// test immediate superclass name
+	public void testSuperClass() {
+		// testing class is a string, so its immediate class is "java.lang.Object"
+	    Object obj="Test String";
+	    Class objClass = obj.getClass();
+	    Inspector objInspector = new Inspector();
+	    String strSuperClass=objInspector.inspectSuperClass(objClass);
+	    org.junit.Assert.assertEquals("java.lang.Object",strSuperClass);
+	    
+	    // testing class is ClassB, so its immediate class "ClassC" is expected
+	    // must have a try catch clause b/c there is a throw in ClassB
+        try{
+        	obj = new ClassB();
+        }
+        catch (Exception e){}
+        objClass = obj.getClass();
+	    strSuperClass=objInspector.inspectSuperClass(objClass);
+	    org.junit.Assert.assertEquals("ClassC",strSuperClass);
+
+	}
 }
